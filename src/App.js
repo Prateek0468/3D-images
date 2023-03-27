0d8b3471081d9bc1c353e03ef25b55b740598a79
Prateek Bhatt <jeevan.bhatt73@gmail.com>
2023-03-27 17:33:37 +0530

    cached images

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:8001/api/images')
-      .then(res => res.json())
-      .then(data => setImages(data));
+    const cachedImages = JSON.parse(localStorage.getItem('images'));
+    if (cachedImages) {
+      setImages(cachedImages);
+    } else {
+      fetchImages();
+    }
   }, []);
-  
+
+  const fetchImages = async () => {
+    try {
+      const response = await fetch('http://localhost:8001/api/images');
+      const data = await response.json();
+      localStorage.setItem('images', JSON.stringify(data));
+      setImages(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>3D Images Viewer</h1>
@@ -23,4 +38,5 @@ function App() {
 }
 
 export default App;
+
 
